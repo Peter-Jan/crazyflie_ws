65c0fbe8629caf2f521cfddb271bde339eea6c33
Peter-Jan <pjan89@gmail.com>
2017-08-20 01:24:39 -0400

    setup

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 sudo apt-get install ros-indigo-desktop-full -y
-gnome-terminal -x bash -c "./setup.py"
+./setup.py
