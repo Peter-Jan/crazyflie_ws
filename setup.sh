8f0cb7dd1fb31959f4af9175bbc16be6f6f7ba36
Peter-Jan <pjan89@gmail.com>
2017-08-20 01:52:20 -0400

    source fix

--- setup.sh
+++ setup.sh
@@ -7,3 +7,11 @@ rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
 catkin_make 
 echo "source ~/crazyflie_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+sudo apt-get install dkms
+sudo git clone https://github.com/paroj/xpad.git /usr/src/xpad-0.4
+sudo dkms install -m xpad -v 0.4
+cd /usr/src/xpad-0.4
+sudo git fetch
+sudo git checkout origin/master
+sudo dkms remove -m xpad -v 0.4 --all
+sudo dkms install -m xpad -v 0.4
