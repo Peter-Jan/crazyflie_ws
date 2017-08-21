7777314b76aa0fec3ed0b41b9095b96313219f0d
Peter-Jan <pjan89@gmail.com>
2017-08-20 21:42:25 -0400

    install fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install ros-indigo-desktop-full -y
+sudo apt-get install ros-indigo-desktop-base -y
 sudo rosdep init
 rosdep update 
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
@@ -10,7 +10,7 @@ source ~/.bashrc
 sudo apt-get install python-rosinstall -y
 rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
 echo "*************************************************************"
-catkin_make 
+./setup.sh
 echo "*************************************************************"
 echo "source ~/crazyflie_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
