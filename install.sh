ab9d8bc06a38765f836f30a439da94059ddf9e72
Peter-Jan <pjan89@gmail.com>
2017-08-22 13:10:41 -0400

    install fix

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ sudo apt-get update
 sudo apt-get install ros-indigo-desktop-full -y
 sudo rosdep init
 rosdep update 
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
-source ~/.bashrc
+source /opt/ros/indigo/setup.bash
 sudo apt-get install python-rosinstall -y
 rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
+catkin_make
+source ~/crazyflie_ws/devel/setup.bash
 sudo apt-get install dkms
 sudo git clone https://github.com/paroj/xpad.git /usr/src/xpad-0.4
 sudo dkms install -m xpad -v 0.4
