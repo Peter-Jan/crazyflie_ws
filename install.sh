09178a92fb862304ac1d21e876a4de704d9d9ded
peter <pjan89@gmail.com>
2017-08-28 23:30:12 -0400

    sublime install

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ sudo cp optirx.py /usr/local/lib/python2.7/site-packages/
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 echo "source ~/crazyflie_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
